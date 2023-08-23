1f713c2f137178e36f85f0a331667c87d24000a8
Zhang Zhuowei <50513641+Cyno2232@users.noreply.github.com>
2023-08-23 20:26:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,8 +13,8 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '天津',
+  CITY: '津南',
 
   // 功能开关,打开：true，关闭：false
   SWITCH: {
